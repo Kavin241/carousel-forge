31374e42e348b769b9724259b20e76496771c1f3
Kavin241 <kavinbhalaji2007@gmail.com>
2026-04-03 14:01:52 +0800

    Fix Canva SDK intents

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,16 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles/panel.css';
 import '@canva/app-ui-kit/styles.css';
+import { prepareDesignEditor } from "@canva/intents/design";
 
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AppUiProvider>
-      <App />
-    </AppUiProvider>
-  </React.StrictMode>
-);
+prepareDesignEditor({
+  render: async () => {
+    createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <AppUiProvider>
+          <App />
+        </AppUiProvider>
+      </React.StrictMode>
+    );
+  }
+});
