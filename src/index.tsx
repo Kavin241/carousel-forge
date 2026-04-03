b90ab3479861df9063062a0e229efdbc9b3a809f
Kavin241 <kavinbhalaji2007@gmail.com>
2026-04-03 14:38:24 +0800

    Dynamically import App to fix Canva SDK undefined design global crash

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { AppUiProvider } from '@canva/app-ui-kit';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
 import './styles/panel.css';
 import '@canva/app-ui-kit/styles.css';
 import { prepareDesignEditor } from "@canva/intents/design";
 
 prepareDesignEditor({
   render: async () => {
+    // Dynamically load the App to ensure window.canva_sdk is fully injected by Canva first!
+    const { App } = await import('./App');
     createRoot(document.getElementById('root')!).render(
       <React.StrictMode>
         <AppUiProvider>
